b35584abdceb415d1b975bc56502e434b31cc114
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2018-11-26 18:54:57 -0600

    Add portal #1

---
+++ GES-PersonalProject/Assets/Scripts/Portal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    private bool isPlayerInTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInTrigger)
+        {
+            Debug.Log("Player activated door.");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
