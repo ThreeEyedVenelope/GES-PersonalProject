bf5e13c5d0c246d3a7b5e75c9555c1ad0cdfe24b
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2018-12-04 10:41:59 -0600

    Add Checkpoint script
    Add checkpoint functionality

--- GES-PersonalProject/Assets/Scripts/Checkpoint.cs
+++ GES-PersonalProject/Assets/Scripts/Checkpoint.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private Color inactivatedColor, activatedColor;
+
+    private bool isActivated = false;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        Color color = inactivatedColor;
+
+        if (isActivated)
+            color = activatedColor;
+
+        spriteRenderer.color = color;
+    }
+
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+        UpdateColor();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the checkpoint.");
+            CharacterController player = collision.GetComponent<CharacterController>();
+            player.SetCurrentCheckpoint(this);
+        }
+    }
 }
