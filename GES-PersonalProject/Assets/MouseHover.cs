fd6cd4cac59209e96e04f024d36e560f2879e9b9
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2018-12-11 10:46:21 -0600

    Add start button functionality

---
+++ GES-PersonalProject/Assets/MouseHover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour {
+
+    public Text hoverText;
+	// Use this for initialization
+	void Start ()
+    {
+        hoverText.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        OnMouseEnter();
+        OnMouseExit();
+	}
+
+    void OnMouseEnter()
+    {
+        hoverText.color = Color.magenta;
+    }
+
+    void OnMouseExit()
+    {
+        hoverText.color = Color.white;
+    }
+}
